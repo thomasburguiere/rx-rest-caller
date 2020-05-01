52b01296bb178a20b3c3d1fdf9fa8d668b1a2c26
Thomas Burguiere <thomas.burguiere@gmail.com>
2020-05-01 20:30:48 +0200

    adding bare tests

---
+++ Tests/RxRestCallerTests/RxRestCallerTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import RxRestCaller
+
+final class RxRestCallerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertNotNil(RxRestCaller())
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
