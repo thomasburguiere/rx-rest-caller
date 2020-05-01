24238f8630c9eadb992654bc419419b09011ff3e
Thomas Burguiere <thomas.burguiere@gmail.com>
2020-05-01 23:37:59 +0200

    adding proper test

--- Tests/RxRestCallerTests/RxRestCallerTests.swift
+++ Tests/RxRestCallerTests/RxRestCallerTests.swift
@@ -1,16 +1,34 @@
 import XCTest
+import RxSwift
 @testable import RxRestCaller
 
 final class RxRestCallerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertNotNil(RxRestCaller())
-    }
+    
+    func test_call_works() {
+          let service = RxRestCaller()
 
+          let actualObservable: Observable<Dictionary<String, Any>> = service.get(url: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+          let ex = self.expectation(description: "Fetching succeeds")
+          _ = actualObservable.subscribe(
+                  onNext: { jsonData in
+                      XCTAssertNotNil(jsonData)
+                      let explanation = jsonData["explanation"]
+                      XCTAssertNotNil(explanation)
+                      print(explanation!)
+                      ex.fulfill()
+                  },
+                  onError: { error in
+                      XCTFail()
+                  },
+                  onCompleted: nil,
+                  onDisposed: nil
+          )
+
+          self.wait(for: [ex], timeout: 10.0)
+      }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("test_call_works", test_call_works),
     ]
 }
