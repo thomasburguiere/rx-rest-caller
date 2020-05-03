d52e09a45776b66d7b6e93aa41b368ef3f5d42ad
Thomas Burguiere <thomas.burguiere@gmail.com>
2020-05-03 17:49:49 +0200

    adding test

--- Tests/RxRestCallerTests/RxRestCallerTests.swift
+++ Tests/RxRestCallerTests/RxRestCallerTests.swift
@@ -4,7 +4,7 @@ import RxSwift
 
 final class RxRestCallerTests: XCTestCase {
     
-    func test_call_works() {
+    func test_get_works() {
           let service = RxRestCaller()
 
           let actualObservable: Observable<Dictionary<String, Any>> = service.get(url: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
@@ -27,8 +27,45 @@ final class RxRestCallerTests: XCTestCase {
 
           self.wait(for: [ex], timeout: 10.0)
       }
+    
+    func test_call_works() {
+        let service = RxRestCaller()
+        
+        var request = URLRequest(url: URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!)
+        request.httpMethod = "GET"
+        
+        let actualObservable: Observable<ResponseWithData> = service.call(urlRequest: request)
+        
+        let ex = self.expectation(description: "Fetching succeeds")
+        _ = actualObservable.subscribe(
+            onNext: { responseWithData in
+                XCTAssertNotNil(responseWithData)
+                XCTAssertNotNil(responseWithData.data)
+                
+                let jsonData : Dictionary<String, Any>
+                do{
+                    try jsonData = JSONSerialization.jsonObject(with: responseWithData.data!) as! Dictionary<String, Any>
+                } catch {
+                    XCTFail()
+                    return
+                }
+                
+                let explanation = jsonData["explanation"]
+                XCTAssertNotNil(explanation)
+                print(explanation!)
+                ex.fulfill()
+        },
+            onError: { error in
+                XCTFail()
+        },
+            onCompleted: nil,
+            onDisposed: nil
+        )
+        
+        self.wait(for: [ex], timeout: 10.0)
+    }
 
     static var allTests = [
-        ("test_call_works", test_call_works),
+        ("test_call_works", test_get_works),
     ]
 }
